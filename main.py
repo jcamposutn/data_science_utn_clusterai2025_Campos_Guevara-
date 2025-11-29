2988e7de7950f7ebf1bdba1d0b7759ad547d8715
jcamposutn <jcamposguevara@frba.utn.edu.ar>
2025-11-28 21:43:20 -0300

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ scripts = [
     "clusterai_joaquin_campos_guevara_machine_learning.py"
 ]
 
-# genero un archivo tipo log que recolecte los outputs de cada archivo
+# genero un archivo tipo log que recolecte los outputs de cada archivo asi queda un registro
 log_file = "output.log"
 
 # abro el archivo en modo escritura y recorro los scripts listados
@@ -23,14 +23,19 @@ with open(log_file, "w") as log:
                 text=True
             )
 
-            # Si corre el EDA, no queremos que imprima todas las comprobaciones realizadas
+            # Si corre el EDA, no queremos que imprima todas las comprobaciones realizadas para la limpieza del dataframe. tampoco queremos ver los resultados parciales el train de ML
             if "clusterai_joaquin_campos_guevara_eda.py" in script.lower():
                 log.write("Limpieza y transformacion del dataframe airbnb_df \n")
             elif "clusterai_joaquin_campos_guevara_manipulaciones.py" in script.lower():
                 log.write("Se entrenó a ambos modelos de ML con los datos \n")
+            # ahora sí mostraremos en el log la prediccion de ambos modelos y su score, ya que es finalmente el objetivo del script
             else:
                 log.write(result.stdout)
                 log.write(result.stderr)
+                # mostramos en consola el resultado de las predicciones de ambos modelos y su score
+                print("\n=== Resultados de predicciones ===")
+                print(result.stdout)
+                print(result.stderr)
 
             log.write(f"\n=== Finalizado {script} ===\n")
 
